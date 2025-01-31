75a84cae238022fb8575e426245d0711ed113bdc
tainur <tainurrahaman31@gmail.com>
2025-01-31 20:18:08 +0600

    made a responsive footer

--- src/Layouts/Homelayout.jsx
+++ src/Layouts/Homelayout.jsx
@@ -1,11 +1,17 @@
 import Banner from "../Components/Banner";
+import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
 
 const Homelayout = () => {
   return (
-    <div className="w-11/12 mx-auto my-4">
-      <Navbar></Navbar>
-      <Banner></Banner>
+    <div>
+      <div className="w-11/12 mx-auto my-4">
+        <Navbar></Navbar>
+        <Banner></Banner>
+      </div>
+      <div>
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
